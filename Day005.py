4d901a133ecb56fd82f7432f8fc7a436ae8e3b34
JusAeng <justo_play@hotmail.com>
2021-08-20 22:53:08 +0700

    Check same digit

--- Day005.py
+++ Day005.py
@@ -1,9 +1,2 @@
-def checkSame(s):
-    for i in range(len(s)):
-        for j in range(len(s)):
-            if i!=j :
-                if s[i]==s[j]:
-                    return True
-    return False
-n=input()
-print(checkSame(n))
+def foundsame(n):
+    print(type(n))
