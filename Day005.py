ac208812d6e62f5393f0ebd098389b22d0e4d4c4
JusAeng <justo_play@hotmail.com>
2021-08-21 16:23:48 +0700

    checkDigitSame

--- Day005.py
+++ Day005.py
@@ -1,2 +1,12 @@
-def foundsame(n):
-    print(type(n))
+def c_duplicate(numb):
+    numb=str(numb)
+    for enumb in numb:
+        count=0
+        for i in range(len(numb)):
+            if enumb == numb[i]:
+                count+=1
+            if count >1:
+                return True
+    return False
+numb = int(input("input: "))
+print(c_duplicate(numb))
