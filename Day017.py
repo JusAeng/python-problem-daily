eb806ee487abb869bcdb0b6b781100023673ef7e
JusAeng <justo_play@hotmail.com>
2021-08-25 15:04:46 +0700

    select8toSum==100

---
+++ Day017.py
@@ -0,0 +1,20 @@
+numbs=[int(x) for x in input("input: ").split()]
+print("output:")
+ans=[]
+
+for i in range(len(numbs)):
+    temp = [x for x in numbs]
+    temp.pop(i)
+    for j in range(len(temp)):
+        ele=temp[j]
+        temp.pop(j)
+        if sum(temp)==100:
+            anslist=[x for x in temp]
+            ans.append(anslist)
+        temp.insert(j,ele)
+    
+if len(ans)!=0:
+    for ele in ans:
+        print(ele)
+else:
+    print("None")
