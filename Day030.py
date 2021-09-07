0316308b93200318d3233a2633302aa2b9680b71
JusAeng <justo_play@hotmail.com>
2021-09-08 00:00:12 +0700

    Hypotenuse

--- Day030.py
+++ Day030.py
@@ -1,8 +1,10 @@
 n=int(input("Enter Input : "))
 ans=n
-
 while ans>0:
     ans-=1
-    for i in range(n):
+    for i in range(2,ans//2):
         b=(ans**2-i**2)**(1/2)
-        
+        if b+ans+i==n:
+            print(ans)
+            ans=0
+            break
