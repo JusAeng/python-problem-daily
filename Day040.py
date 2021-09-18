64688613b7b8b882e14e7941ebdadb6ffa68d47f
JusAeng <justo_play@hotmail.com>
2021-09-18 12:20:27 +0700

    anagram

--- Day040.py
+++ Day040.py
@@ -1,2 +1,11 @@
-s=[]
-print(bool(s))
+s1,s2=[input().lower() for x in range(2)]
+s1=s1.replace(" ","")
+s2=s2.replace(" ","")
+lst1=[x for x in s1]
+for j,i in enumerate(s2):
+    if i in lst1 and len(s1)==len(s2):
+        lst1.remove(i)
+    else:
+        print("NO")
+        break
+else: print("YES")
